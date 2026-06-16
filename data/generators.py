85dd914bc822f192925be85f01d3698fcefa8f76
Igor Fedotov <igor2505199@ya.ru>
2026-06-16 08:23:20 +0300

    Федотов Игорь

--- data/generators.py
+++ data/generators.py
@@ -1,11 +1,9 @@
 import random
 import string
 
-
 def generate_email():
     number = random.randint(100, 999)
     return f"test_testov_13_{number}@yandex.ru"
-
 
 def generate_password(length=8):
     chars = string.ascii_letters + string.digits
